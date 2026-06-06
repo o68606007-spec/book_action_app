980e689d830cf92203bc33c299dfa6ac33d398c5
o68606007-spec <test@DESKTOP-SRSUEFO.localdomain>
2026-06-06 15:38:56 +0900

    Router / add

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -13,6 +13,7 @@ export const Router: FC = memo(() => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/home" element={
